29797c75a47cd1dd511bc5883cd286ad03ac5d9e
YJazzar <YJazzar@users.noreply.github.com>
2024-12-29 23:48:48 -0500

    Adding a test table that shows the linear commit history

--- frontend/src/pages/repo/RepoHomeView.tsx
+++ frontend/src/pages/repo/RepoHomeView.tsx
@@ -1,11 +1,38 @@
 import { Button } from "@/components/ui/button";
-import { OpenNewRepo } from "../../../wailsjs/go/backend/App";
+import { OpenNewRepo, RunGitLog } from "../../../wailsjs/go/backend/App";
+import { useState } from "react";
+import { useParams } from "react-router";
+import { backend } from "wailsjs/go/models";
 
 
 export default function RepoHomeView() {
+	const { encodedRepoPath } = useParams();
+	const [logs, setLogs] = useState<backend.GitLogCommitInfo[]>([]);
+
+	if (!encodedRepoPath) {
+		return <>Error: why are we rendering RepoHomeView when there's no repo provided?</>;
+	}
+
+	const repoPath = atob(encodedRepoPath);
+
+	const refreshLogs = async () => {
+		console.debug("refreshing logs on ", repoPath)
+		const newLogs = await RunGitLog(repoPath);
+		console.debug("got: ", newLogs)
+		setLogs(newLogs)
+	};
+
 	return (
-		<div className="">
-			Repo Log view here
-		</div>
+		<>
+			<Button onClick={refreshLogs}>Refresh </Button>
+			Log results:
+			{logs.map((log) => {
+				return (
+					<div key={log.commitHash}>
+						<code className="whitespace-pre-wrap">{JSON.stringify(log, null, 3)}</code>
+					</div>
+				);
+			})}
+		</>
 	);
 }
--- frontend/src/pages/repo/RepoLogView.tsx
+++ frontend/src/pages/repo/RepoLogView.tsx
@@ -1,11 +1,119 @@
-import { Button } from "@/components/ui/button";
-import { OpenNewRepo } from "../../../wailsjs/go/backend/App";
+import { Button } from '@/components/ui/button';
+import useCommitGraphBuilder from '@/hooks/use-commit-graph-builder';
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
+import { useState } from 'react';
+import { useParams } from 'react-router';
+import { backend } from 'wailsjs/go/models';
+import { RunGitLog } from '../../../wailsjs/go/backend/App';
 
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 
 export default function RepoLogView() {
+	const { encodedRepoPath } = useParams();
+	const [logs, setLogs] = useState<backend.GitLogCommitInfo[]>([]);
+
+	const commitGraph = useCommitGraphBuilder(logs);
+
+	if (!encodedRepoPath) {
+		return <>Error: why are we rendering RepoHomeView when there's no repo provided?</>;
+	}
+
+	const repoPath = atob(encodedRepoPath);
+
+	const refreshLogs = async () => {
+		console.debug('refreshing logs on ', repoPath);
+		const newLogs = await RunGitLog(repoPath);
+		console.debug('got: ', newLogs);
+		setLogs(newLogs);
+	};
+
+	console.debug(commitGraph);
+
 	return (
-		<div className="">
-			Repo Log view here
+		<>
+			<Button onClick={refreshLogs}>Refresh </Button>
+			Log results:
+			<LinearCommitLogTable columns={columns} data={logs} />
+			{/* {logs.map((log) => {
+				return (
+					<div key={log.commitHash}>
+						<code className="whitespace-pre-wrap">{JSON.stringify(log, null, 3)}</code>
+					</div>
+				);
+			})} */}
+		</>
+	);
+}
+
+interface DataTableProps<TData, TValue> {
+	columns: ColumnDef<TData, TValue>[];
+	data: TData[];
+}
+
+function LinearCommitLogTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
+	const table = useReactTable({
+		data,
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+	});
+
+	return (
+		<div className="rounded-md border">
+			<Table>
+				<TableHeader>
+					{table.getHeaderGroups().map((headerGroup) => (
+						<TableRow key={headerGroup.id}>
+							{headerGroup.headers.map((header) => {
+								return (
+									<TableHead key={header.id}>
+										{header.isPlaceholder
+											? null
+											: flexRender(header.column.columnDef.header, header.getContext())}
+									</TableHead>
+								);
+							})}
+						</TableRow>
+					))}
+				</TableHeader>
+				<TableBody>
+					{table.getRowModel().rows?.length ? (
+						table.getRowModel().rows.map((row) => (
+							<TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+								{row.getVisibleCells().map((cell) => (
+									<TableCell key={cell.id}>
+										{flexRender(cell.column.columnDef.cell, cell.getContext())}
+									</TableCell>
+								))}
+							</TableRow>
+						))
+					) : (
+						<TableRow>
+							<TableCell colSpan={columns.length} className="h-24 text-center">
+								No results.
+							</TableCell>
+						</TableRow>
+					)}
+				</TableBody>
+			</Table>
 		</div>
 	);
 }
+
+const columns: ColumnDef<backend.GitLogCommitInfo>[] = [
+	{
+		accessorKey: 'commitHash',
+		header: 'hash',
+	},
+	{
+		accessorKey: 'username',
+		header: 'User',
+	},
+	{
+		accessorKey: 'shortStat',
+		header: 'Stat',
+	},
+	{
+		accessorKey: 'commitMessage',
+		header: 'Message',
+	},
+];
